aad580425f0e86f703c85df086b01ef375290705
Peter Szasz <szasz.peter@ymail.com>
2022-07-12 13:50:48 +0200

    Initial scaffolding of the project.
    This commit contains:
    1. Necessary dependencies imported from the Basic Tools App.
    2. App, Data, Domain, Presentation Modules

---
+++ app/src/main/java/com/codarchy/cleanarchitecture/CleanArchitectureApplication.kt
@@ -0,0 +1,8 @@
+package com.codarchy.cleanarchitecture
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CleanArchitectureApplication: Application() {
+}
